27dd3ba0e628162cd192a73e3f74605c208c2d72
zoharsanchez <lorena.zohar@gmail.com>
2016-07-24 00:38:53 -0700

    Rebase current changes - add styling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,22 @@
 var App = props => (
-  <div>
-    <header> 
-      <h1><Link to='/landing'>Wonder Wander</Link></h1>
-      <nav>
-        <button><Link to='/login'>Log in</Link></button>
-        <button><Link to='/signup'>Sign up</Link></button>
-      </nav>
-    </header>
-    {props.children}
-  </div>
+  <nav>
+    <div className='nav-wrapper'>
+      <div className='row blue lighten-2 z-depth-1'>
+        <div className='col s4'>
+          <a className='logo'><Link to='/landing'>Wonder Wander <i className='fa fa-paper-plane-o' aria-hidden='true'></i></Link></a>
+        </div>
+        <div className='col s8'>
+          <div className='right'>
+            <ul>
+              <li><Link to='/itineraries'>Itineraries</Link></li>
+              <li><Link to='/submit'>Create new itinerary</Link></li>
+              <li><Link to='/login'>Login</Link></li>
+              <li><Link to='/signup'>Sign up</Link></li>
+            </ul>
+            {props.children}
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
 );
