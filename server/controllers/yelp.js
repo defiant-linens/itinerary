8374a0cb29c501045c4d719414cba1acfb64d82b
Pyry Kovanen <pyry.kovanen@gmail.com>
2016-07-24 17:45:40 -0700

    Add Yelp API integration

---
+++ server/controllers/yelp.js
@@ -0,0 +1,67 @@
+/* require the modules needed */
+var oauthSignature = require('oauth-signature');
+var n = require('nonce')();
+var request = require('request');
+var qs = require('querystring');
+var _ = require('lodash');
+var secrets = require('./secrets').secrets;
+
+/* Function for yelp call
+ * ------------------------
+ * set_parameters: object with params to search
+ * callback: callback(error, response, body)
+ */
+exports.requestYelp = function(set_parameters, callback) {
+
+  /* The type of request */
+  var httpMethod = 'GET';
+
+  /* The url we are using for the request */
+  var url = 'http://api.yelp.com/v2/search';
+
+  /* We can setup default parameters here */
+  var default_parameters = {
+    location: 'San+Francisco',
+    sort: '2'
+  };
+
+  /* We set the require parameters here */
+  var required_parameters = {
+    oauth_consumer_key : secrets.oauth_consumer_key,
+    oauth_token : secrets.oauth_token,
+    oauth_nonce : n(),
+    oauth_timestamp : n().toString().substr(0,10),
+    oauth_signature_method : 'HMAC-SHA1',
+    oauth_version : '1.0'
+  };
+
+  /* We combine all the parameters in order of importance */ 
+  var parameters = _.assign(default_parameters, set_parameters, required_parameters);
+
+  /* We set our secrets here */
+  var consumerSecret = secrets.consumerSecret;
+  var tokenSecret = secrets.tokenSecret;
+
+  /* Then we call Yelp's Oauth 1.0a server, and it returns a signature */
+  /* Note: This signature is only good for 300 seconds after the oauth_timestamp */
+  var signature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
+
+  /* We add the signature to the list of paramters */
+  parameters.oauth_signature = signature;
+
+  /* Then we turn the paramters object, to a query string */
+  var paramURL = qs.stringify(parameters);
+
+  /* Add the query string to the url */
+  var apiURL = url+'?'+paramURL;
+
+  /* Then we use request to send make the API Request */
+  request(apiURL, function(error, response, body){
+    return callback(error, response, body);
+  });
+};
+
+
+exports.requestYelp(options, function(err, resp, body) {
+  console.log(JSON.parse(body));
+});
