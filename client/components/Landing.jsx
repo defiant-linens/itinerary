e4aff5ad703ed94e0d10325894dbb5dc90cf6c71
zoharsanchez <lorena.zohar@gmail.com>
2016-07-24 20:31:04 -0700

    Basic styling

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,13 +1,9 @@
 var LandingView = () => (
-  <div className='landing'>
-
-    <div className='navOptions'>
-      <button><Link to='/choose-planner'>'Create Itinerary'</Link></button>
-      <button><Link to='/itineraries'>'View My Itineraries'</Link></button>
-    </div>
-
-    <div className="itineraries">
-      <ItineraryView />
-    </div>
-  </div>
+	<div className='container'>
+		<div className='jumbotron'>
+			<h1 className='ole'>Wonder Wander <i className='fa fa-paper-plane-o smLogo' aria-hidden='true'></i></h1>
+			<Link to='/choose-planner' className='btn btn-success'>Create Itinerary</Link><span>   </span>
+			<Link to='/itineraries' className='btn btn-success'>View My Itineraries</Link>
+		</div>
+	</div>
 );
---
+++ server/controllers/secrets.js
@@ -0,0 +1,6 @@
+exports.secrets = {
+  oauth_consumer_key:'Jgp6M4OIQMqQfHakvyc2HA',
+  oauth_token:'7sRqrt9XMbukjv9KdpwcOTz8imNG6Nd1',
+  consumerSecret:'8Vk6Cctpu8ebrE7HZMDCygf_Uko',
+  tokenSecret:'T8ZK8PtUQdwSeE3ccVJSwaebhWI'
+};
