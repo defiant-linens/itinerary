ec4d1baaf5397b34143762eb38d343260a983bf3
zoharsanchez <lorena.zohar@gmail.com>
2016-07-25 13:38:39 -0700

    Add styling to EventView

--- client/components/EventView.jsx
+++ client/components/EventView.jsx
@@ -1,27 +1,33 @@
 var EventView = (props) => {
-  console.log(props)
-
+  console.log('this are the events', props.yelpEvents)
   if (props.yelpEvents.length > 0) {
     var d = props.day;
     var e = props.eventID;
     var index = 3 * (d - 1) + e;
     var event = props.yelpEvents[index];
 
-    if (event) {
-      var renderTags = <div>
+    var renderTags = <div className="panel panel-success">
+      <div className="panel-heading">
+        <h5 class="panel-title"><a className="white-text" href={event.url}>{event.name}</a></h5>
+      </div>
+      <div className="panel-body">
         <div>
-          {event.name}
+          <p><img src={event.image_url} className="thumbnail"/></p>
         </div>
         <div>
-          {event.url}
+          <p><img src={event.rating_img_url}/></p>
         </div>
         <div>
-          {event.rating}
+          <p><strong>Address: </strong> {event.location.display_address[0]}, {event.location.display_address[1]}</p>
         </div>
+        <div>
+          <p><strong>Review: </strong> {event.snippet_text}</p>
+        </div>
+        <div>
+          <p><strong>Categories: </strong>{event.categories.map(category => <span className="label label-success category-right">{category[0]}</span>)}</p>
+        </div>
+      </div>
       </div>;
-    } else {
-      renderTags = null;
-    }
 
   } else {
     var renderTags = null;
@@ -29,10 +35,8 @@ var EventView = (props) => {
 
   return (
     <div>
-      <div>
-        Event:
-      </div>
       {renderTags}
     </div>
   );
 }
+
