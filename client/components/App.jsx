5f8bf74335e454ae00318cb69fc04926ef0c41f8
zoharsanchez <lorena.zohar@gmail.com>
2016-07-25 15:40:30 -0700

    Add logout button and functionality that prevent logged out user from viewing itineraries

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,41 @@
-var App = props => (
-  <div className='container'>
-    <div className='header'>
-      <nav className="navbar navbar-inverse navbar-fixed-top">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <Link to='/landing' className='navbar-brand logo'>Wonder Wander <i className='fa fa-paper-plane-o' aria-hidden='true'></i></Link>
-          </div>
+var App = props => {
+  var userLogout = event => {
+    window.user = undefined;
+    fetch('/classes/logout', {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      credentials: 'same-origin',
+      method: 'GET' 
+    })
+    .then(() => {
+      console.log('Successful clientside GET request');
+    })
+    .catch(err => {
+      console.log('Logout request error:', err);
+    });
+  };
 
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav navbar-right">
-              <li><Link to='/login'>Login</Link></li>
-              <li><Link to='/signup'>Sign up</Link></li>
-            </ul>
+  return (
+    <div className='container'>
+      <div className='header'>
+        <nav className="navbar navbar-inverse navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <Link to='/landing' className='navbar-brand logo'>Wonder Wander <i className='fa fa-paper-plane-o' aria-hidden='true'></i></Link>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav navbar-right">
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/signup'>Sign up</Link></li>
+                <li><Link to='/landing' onClick={userLogout}>Logout</Link></li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-      {props.children}
+        </nav>
+        {props.children}
+      </div>
     </div>
-  </div>
-);
+  );
+};
