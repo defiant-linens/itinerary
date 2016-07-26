bdf8a092dce5760cc8247d4cfd045278762f859a
Billy Lan <billylan22@gmail.com>
2016-07-26 11:30:35 -0700

    fixed untracked files

--- server/controllers/secrets.js
+++
@@ -1,6 +0,0 @@
-exports.secrets = {
-  oauth_consumer_key:'Jgp6M4OIQMqQfHakvyc2HA',
-  oauth_token:'7sRqrt9XMbukjv9KdpwcOTz8imNG6Nd1',
-  consumerSecret:'8Vk6Cctpu8ebrE7HZMDCygf_Uko',
-  tokenSecret:'T8ZK8PtUQdwSeE3ccVJSwaebhWI'
-};
