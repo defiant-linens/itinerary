78890fe01c5ca8ccb268f6066e56b11608804c93
Billy Lan <billylan22@gmail.com>
2016-07-27 16:54:35 -0700

    only save itinerary when user clicks save

--- client/components/Summary.jsx
+++ client/components/Summary.jsx
@@ -1,18 +1,9 @@
 var Summary = ({data}) => {
-
-  var summary = data.overview;
-  if (data.overview.length > 75) {
-    summary = data.overview.substring(0, 76) + '...';
-  }
-  console.log('this is data:', data);
-
   // Note that the image in return is hardcoded for now
   return (
     <div className="summary">
       <img src="http://sites.msdwt.k12.in.us/jfeeney/wp-content/uploads/sites/15/2014/07/worldwide-travel-nurse-advantages.jpg" />
       <h2>{data.location + ' by ' + data.User.name}</h2>
-      <p>{summary}</p>
     </div>
   );
-
 };
